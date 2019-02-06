343d1d5c38de4218e1bd92484774ab04654cc843
DaSilva123 <mateusma1999@gmail.com>
2019-02-06 12:03:34 -0200

    trabalho com marvem

---
+++ Web-Trabalho-final/src/main/java/usuario/Usuario.java
@@ -0,0 +1,71 @@
+package usuario;
+
+public class Usuario {
+	
+	private String nome;
+	private String sobrenome;
+	private String Sexo;
+	private String email;
+	private String Telefone;
+	private String Endereco;
+	private String Bairro;
+	private String Cidade;
+	private String Estado;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getSexo() {
+		return Sexo;
+	}
+	public void setSexo(String sexo) {
+		Sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(String telefone) {
+		Telefone = telefone;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	public String getBairro() {
+		return Bairro;
+	}
+	public void setBairro(String bairro) {
+		Bairro = bairro;
+	}
+	public String getCidade() {
+		return Cidade;
+	}
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+	public String getEstado() {
+		return Estado;
+	}
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	
+	
+
+}
